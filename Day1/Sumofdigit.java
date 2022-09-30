058c6c11221612ae085ba917bd0d23eeeff3db18
Harsh-Trivedii <114759277+Harsh-Trivedii@users.noreply.github.com>
2022-09-30 21:51:11 +0530

    Create Sumofdigit.java

---
+++ Day1/Sumofdigit.java
@@ -0,0 +1,15 @@
+public class Sumofdigit
+{
+  public static void main(String args[])
+  {
+    int n=43212;
+    int rem,sum=0;
+    while(n!=0)
+    {
+      rem=n%10;
+      sum=sum+rem;
+      n=n/10;
+    }
+    System.out.println("Sum of all digits="+sum);
+  }
+}
