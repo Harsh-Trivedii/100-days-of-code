7a2317580e1460e1ed5e6eabc7beaa41b71c1331
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2022-10-02 10:28:29 +0530

    Create starpattern2.java

---
+++ Day 3/starpattern2.java
@@ -0,0 +1,25 @@
+/* printing reverse star pattern
+*****
+****
+***
+**
+*
+*/
+public class starpattern2
+{
+public static void main(String args[])
+{
+int i,j;
+int n=5;
+for(i=1;i<=n;i++)
+{
+for(j=5;j>=i;j--)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+
+
+}
+}
