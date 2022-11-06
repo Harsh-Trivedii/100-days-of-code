632d09fa0e58341582950395c983375c3f772991
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2022-11-06 13:46:02 +0530

    Create minimumcharacteradd.java

---
+++ Day 38/minimumcharacteradd.java
@@ -0,0 +1,41 @@
+/*
+Question-Given string str of length N. The task is to find the minimum characters to be added at the front to make string palindrome.
+
+Example-
+Input:
+S = "abc"
+Output: 2
+Explanation: 
+Add 'b' and 'c' at front of above string to make it
+palindrome : "cbabc"
+
+Constraints:
+1 <= S.length <= 106
+
+Test cases: 576/576.
+*/
+
+class Solution {
+    public static int minChar(String str) {
+       //Write your code here
+         int count=0;
+         int i=0;
+         int n=str.length()-1;
+         int j=n;
+            while(j>i)
+            {
+                if(str.charAt(i)!=str.charAt(j))
+                {
+                    count++;
+                    j=--n;
+                    i=0;
+                }
+                else
+                {
+                    i++;
+                    j--;
+                }
+            }
+        return count;
+    }
+}
