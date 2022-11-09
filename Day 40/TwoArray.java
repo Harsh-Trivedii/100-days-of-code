1a7cca0f1d96d3aaada768aa3fc5890569b714af
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2022-11-09 19:18:58 +0530

    Update and rename Solution.java to TwoArray.java

--- Day 40/TwoArray.java
+++ Day 40/TwoArray.java
@@ -23,8 +23,45 @@ Constraints:
 
 Test cases: 10126 / 10126
 */
+
 // main class is predefined in the question.
 
+//{ Driver Code Starts
+import java.io.*;
+
+import java.util.*;
+class TwoArray {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		long t=sc.nextLong();
+		
+		while(t-->0)
+		{
+		  int n=sc.nextInt();
+		  long arr[]=new long[n];
+		  long brr[]=new long[n];
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      arr[i]=sc.nextLong();
+		  }
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      brr[i]=sc.nextLong();
+		  }
+		  Solution ob = new Solution();
+		  System.out.println(ob.check(arr,brr,n)==true?"1":"0");
+		  
+		  
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
 class Solution{
     //Function to check if two arrays are equal or not.
     public static boolean check(long A[],long B[],int N)
