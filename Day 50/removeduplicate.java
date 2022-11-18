a6c3b1202057f863a8ca137ac7ec43f8a4425881
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2022-11-18 21:22:40 +0530

    Create removeduplicate.java

---
+++ Day 50/removeduplicate.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class removeduplicate {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine();
+            
+            Solution ob = new Solution();
+            String result = ob.removeDups(s);
+            
+            System.out.println(result);
+        }
+    }
+}
+class Solution {
+    String removeDups(String S) {
+        int index = 0;
+        char str[]=S.toCharArray();
+        int n=str.length;
+        for (int i = 0; i < n; i++)
+        {
+ 
+            int j;
+            for (j = 0; j < i; j++)
+            {
+                if (str[i] == str[j])
+                {
+                    break;
+                }
+            }
+            if (j == i)
+            {
+                str[index++] = str[i];
+            }
+        }
+        return String.valueOf(Arrays.copyOf(str, index));
+    }
+}
