2fde9933a01629d3fba7352849fc90dce07da882
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2022-11-27 22:27:47 +0530

    Create Array.java

---
+++ Day 59/Array.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		
+		while(t-->0){
+		    
+		    //input size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //inserting elements in the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    StringBuffer str = new StringBuffer();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		  
+		    //calling leaders() function
+		    res = obj.leaders(arr, n);
+		    
+		    //appending result to a String
+		    for(int i=0; i<res.size(); i++){
+		        str.append(res.get(i)+" ");
+		    }
+		    
+		    //printing the String
+		    System.out.println(str);
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        ArrayList<Integer>a=new ArrayList<Integer>();
+       Stack<Integer> stack = new Stack<Integer>();
+       int size=arr.length;
+        stack.push(arr[size - 1]);
+ 
+        for (int i = size - 2; i >= 0; i--) {
+            if (arr[i] >= stack.peek()) {
+                stack.push(arr[i]);
+            }
+        }
+ 
+        while (!stack.empty()) {
+            a.add(stack.pop());
+        }
+        return a;
+    }
+}
